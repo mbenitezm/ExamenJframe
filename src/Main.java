a74ee0b47adbe6cb3556472c5667243fd6523575
Marcel <Marcel@10.20.8.133>
2014-09-24 12:36:54 -0500

    El examen de Applet cambiado a JFrame con pausa, guardado y cargado

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marcel
+ */
+public class Main {
+        public static void main(String[] args) {
+            JFrameExamen jfrProbando = new JFrameExamen();
+            jfrProbando.setVisible(true);
+            jfrProbando.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
